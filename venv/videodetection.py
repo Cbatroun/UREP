8208a905a1aedd10906338e22f08abca3e2d007d
Christopher <44477665+Cbatroun@users.noreply.github.com>
2020-04-02 20:44:08 -0400

    added in frame by frame processing

--- venv/videodetection.py
+++ venv/videodetection.py
@@ -1,10 +1,23 @@
-from imageai.Detection import VideoObjectDetection
 import os
-
-detector = VideoObjectDetection()
+import cv2
+import time
+#constants
 dirpath = os.getcwd()
-print(dirpath)
+frame_dir = os.path.join(dirpath,"Frames", "frame.png")
+print(frame_dir)
+detector = ObjectDetection()
+#print(dirpath)
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(dirpath, '..', "model\\yolo.h5"))
 detector.loadModel()
-detection = detector.deromVideo(input_file_path= os.path.join(dirpath, "traffic-mini.mp4"), output_file_path=os.path.join(dirpath, "traffic_mini_detected_1.mp4"),log_progress=true)tectObjectsF
+camera = cv2.VideoCapture(os.path.join(dirpath, "traffic-mini.mp4"))
+time.perf_counter()
+while True:
+    ret, frame = camera.read()
+    cv2.imwrite(frame_dir, frame)
+    detection = detector.detectObjectsFromImage(input_image=frame_dir, output_image_path=frame_dir)
+    #frame_proc = cv2.imread(frame_dir)
+    #cv2.imshow("AI stream", cv2.resize(frame_proc, (800,600)))
+    #if cv2.waitKey(25) & 0xFF == ord('q'):
+        #cv2.destroyAllWindows()
+        #break
