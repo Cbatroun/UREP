f7337f81bd6e5e04d10876c3b956b5c59f0a40f3
Christopher <44477665+Cbatroun@users.noreply.github.com>
2020-04-02 23:10:15 -0400

    Update
    increased speed by removing, opencv over head by passing array images instead of file paths to the detection algorithm. Also running Yolov3 in fastest mode

--- venv/videodetection.py
+++ venv/videodetection.py
@@ -1,25 +1,29 @@
 from imageai.Detection import ObjectDetection
 import os
 import cv2
-import time
+import timeit
 
 #constants
 dirpath = os.getcwd()
-frame_dir = os.path.join(dirpath,"Frames", "frame.png")
-print(frame_dir)
+
 detector = ObjectDetection()
 #print(dirpath)
 detector.setModelTypeAsYOLOv3()
 detector.setModelPath(os.path.join(dirpath, '..', "model\\yolo.h5"))
-detector.loadModel()
-camera = cv2.VideoCapture(os.path.join(dirpath, "traffic-mini.mp4"))
+detector.loadModel("fast")
+custom = detector.CustomObjects(car=True)
 
+camera = cv2.VideoCapture(os.path.join(dirpath, "videoplayback.mp4"))
+#camera = cv2.VideoCapture(0)
+tracker=0
 while True:
+    #tracker=tracker +1
     ret, frame = camera.read()
-    cv2.imwrite(frame_dir, frame)
-    detection = detector.detectObjectsFromImage(input_image=frame_dir, output_image_path=frame_dir)
-    frame_proc = cv2.imread(frame_dir)
+    frame_proc,detection = detector.detectCustomObjectsFromImage(input_image=frame, custom_objects=custom, input_type="array", output_type="array")
+
     cv2.imshow("AI stream", cv2.resize(frame_proc, (800,600)))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+
+
