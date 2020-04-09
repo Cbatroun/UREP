618e7c39abee12382cfabd39c5a50e4bcc0c2203
Christopher <44477665+Cbatroun@users.noreply.github.com>
2020-04-09 13:14:34 -0400

    MultiProcessing Framework part1
    implemented the first steps of multiprocessing and was able to pass the detection list through processes via a process stack
    things to do:
    there is a thread leak somewhere and after hitting q to shut the video processing of, python does not get exit code
    Work on having the new process continually keep up with the main detection thread by possibly looping with a boolean check to shut the thread dwon if video processing ends.

--- venv/videodetection.py
+++ venv/videodetection.py
@@ -1,7 +1,7 @@
 from imageai.Detection import ObjectDetection
+from multiprocessing import Process, Queue
 import os
 import cv2
-import timeit
 
 #constants
 dirpath = os.getcwd()
@@ -13,17 +13,27 @@ detector.setModelPath(os.path.join(dirpath, '..', "model\\yolo.h5"))
 detector.loadModel("fast")
 custom = detector.CustomObjects(car=True)
 
-camera = cv2.VideoCapture(os.path.join(dirpath, "videoplayback.mp4"))
+camera = cv2.VideoCapture(os.path.join(dirpath, "parkingvideo.mp4"))
 #camera = cv2.VideoCapture(0)
-tracker=0
-while True:
-    #tracker=tracker +1
-    ret, frame = camera.read()
-    frame_proc,detection = detector.detectCustomObjectsFromImage(input_image=frame, custom_objects=custom, input_type="array", output_type="array")
-
-    cv2.imshow("AI stream", cv2.resize(frame_proc, (800,600)))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+tracker = 0
 
 
+def detect(q):
+    while True:
+        #tracker=tracker +1
+        ret, frame = camera.read()
+        frame_proc,detection = detector.detectCustomObjectsFromImage(input_image=frame, custom_objects=custom, input_type="array", output_type="array")
+        q.put(detection)
+        cv2.imshow("AI stream", cv2.resize(frame_proc, (800, 600)))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=detect, args=(q,))
+    p.start()
+    for x in q.get():
+        print(x)
+    p.join()
