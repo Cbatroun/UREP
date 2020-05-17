75c4deb65ab1baf7975d8484c61f3d3c4b02abc6
Ashazi <azewilous@gmail.com>
2020-05-17 17:11:44 -0400

    car moving algorithm

--- venv/videodetection.py
+++ venv/videodetection.py
@@ -2,6 +2,7 @@ from imageai.Detection import ObjectDetection
 from multiprocessing import Process, Queue
 import os
 import cv2
+import math
 
 # constants
 dirpath = os.getcwd()
@@ -13,15 +14,12 @@ detector.setModelPath(os.path.join(dirpath, '..', "model\\yolo.h5"))
 detector.loadModel("fast")
 custom = detector.CustomObjects(car=True)
 
-camera = cv2.VideoCapture(os.path.join(dirpath, "parkingvidoe01.mp4"))
+camera = cv2.VideoCapture(os.path.join(dirpath, "parkingvideo_Trim.mp4"))
 # camera = cv2.VideoCapture(0)
 tracker = 0
 
 running = 1
 curr_frame = 0
-
-# def dist(xr, ys):
-#     return math.sqrt(sum([(a - b) ** 2 for a, b in zip(xr, ys)]))
 
 
 def detect(q, f):
@@ -36,46 +34,56 @@ def detect(q, f):
                                                                           input_type="array", output_type="array")
             q.put(detection)
 
-            cv2.imshow("AI stream", cv2.resize(frame_proc, (800, 600)))
+            cv2.imshow("AI stream", cv2.resize(frame_proc, (1200, 800)))
             # initial = 1
             c += 1
             f.put(c)
         if cv2.waitKey(25) & 0xFF == ord('l'):
-            # tracker=tracker +1
-            ret, frame = camera.read()
-            frame_proc, detection = detector.detectCustomObjectsFromImage(input_image=frame, custom_objects=custom,
+            skip = 0
+            while skip < 40:
+                # tracker=tracker +1
+                ret, frame = camera.read()
+                frame_proc, detection = detector.detectCustomObjectsFromImage(input_image=frame, custom_objects=custom,
                                                                           input_type="array", output_type="array")
-            q.put(detection)
+                q.put(detection)
 
-            cv2.imshow("AI stream", cv2.resize(frame_proc, (800, 600)))
-            c += 1
-            f.put(c)
+                cv2.imshow("AI stream", cv2.resize(frame_proc, (1200, 800)))
+                c += 1
+                skip += 1
+                f.put(c)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             running = 0
             break
 
 
+def distance(a1, b1, a2, b2):
+    return math.sqrt((a2 - a1)**2 + (b2 - b1)**2)
+
+
 class Point:
-    def __init__(self, x, y, p_id):
-        self.x = x
-        self.y = y
-        self.p_id = p_id + 1
+    def __init__(self, xp, yp, c):
+        self.x = xp
+        self.y = yp
+        self.timer = 0
+        self.c = c
+        # self.nextTo = []
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
     def __str__(self):
-        return "Point(id=" + str(self.p_id) + ",x=" + str(self.x) + ",y=" + str(self.y) + ")"
+        return "Point(percent=" + str(self.c) + ",x=" + str(self.x) + ",y=" + str(self.y) \
+               + ", timer=" + str(self.timer) + ")"
 
 
-frames = {
+centroids = {
     '0': {
-        'points': [Point(0, 0, -1)]
+        'points': [Point(0, 0, 0)]
     }
 }
 
-print("0,", *frames["0"]['points'])
+print("0,", *centroids["0"]['points'])
 
 if __name__ == '__main__':
     q = Queue()
@@ -87,21 +95,72 @@ if __name__ == '__main__':
     while running == 1:
         cf = str(f.get())
         for g in q.get():
-            if g['percentage_probability'] > 81:
+            percentage = g['percentage_probability']
+            x1 = g['box_points'][0]
+            y1 = g['box_points'][1]
+            x2 = g['box_points'][2]
+            y2 = g['box_points'][3]
+            if percentage > 70:
                 # print(x)
-                xc = (g['box_points'][0] + g['box_points'][2]) / 2
-                yc = (g['box_points'][1] + g['box_points'][3]) / 2
+                xc = (x1 + x2) / 2
+                yc = (y1 + y2) / 2
                 # print("center x: ", xc.__str__(), " -  y: " + yc.__str__())
-                if cf in frames.keys():
-                    p = frames[cf]['points']
-                    p.append(Point(xc, yc, p[p.__len__() - 1].p_id))
+                if cf in centroids.keys():
+                    p = centroids[cf]['points']
+                    p.append(Point(xc, yc, percentage))
                 else:
-                    frames[cf] = {
-                        'points': [Point(xc, yc, -1)]
+                    centroids[cf] = {
+                        'points': [Point(xc, yc, percentage)]
                     }
 
-        if frames.__len__() > 3:
+        if centroids.__len__() > 3:
             rf = str(int(cf) - 3)
-            frames.pop(rf)
+            centroids.pop(rf)
 
-        print(str(cf) + ",", *frames[str(cf)]['points'])
+        cs = centroids[cf]['points']
+        if centroids.__len__() > 2:
+            cs_pre = centroids[str(int(cf) - 1)]['points']
+            for p in cs_pre:
+                found = False
+                for c in cs:
+                    close = distance(p.x, p.y, c.x, c.y)
+                    if close < 15:
+                        found = True
+                if not found:
+                    p.timer += 1
+                    centroids[cf]['points'].append(p)
+                    if p.timer > 20:
+                        print('Car moved')
+                        centroids[cf]['points'].remove(p)
+
+        # cs = centroids[cf]['points']
+        # length = cs.__len__()
+        # for i in range(length):
+        #     # for j in range(i + 1, length):
+        #     #     a1 = cs[i].x
+        #     #     b1 = cs[i].y
+        #     #     a2 = cs[j].x
+        #     #     b2 = cs[j].y
+        #     #     dist = distance(a1, b1, a2, b2)
+        #     #     if dist < 155:
+        #     #         if not cs[i].nextTo.__contains__(dist):
+        #     #             cs[i].nextTo.append(dist)
+        #     #         if not cs[j].nextTo.__contains__(dist):
+        #     #             cs[j].nextTo.append(dist)
+        #     found = False
+        #     if centroids.__len__() > 2:
+        #         cs_pre = centroids[str(int(cf) - 1)]['points']
+        #         for csp in cs_pre:
+        #             close = distance(cs[i].x, cs[i].y, csp.x, csp.y)
+        #             if close < 15:
+        #                 found = True
+        #         if not found:
+        #             if cs[i].timer > 20:
+        #                 print('Car moved')
+        #             else:
+        #                 cs[i].timer = cs_pre[i]
+
+        # print(str(cf) + ",", *centroids[str(cf)]['points'])
+        print("frame " + cf)
+        for d in cs:
+            print(d)
